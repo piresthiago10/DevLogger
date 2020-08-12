3ef9c6ffd94909c585064c348a2b9bac13e39209
Thiago Pires <thiago.pires@sectrans.com.br>
2020-08-12 17:38:08 -0300

    release - crud

--- devlogger/src/app/app.module.ts
+++ devlogger/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
 import { LogsComponent } from './components/logs/logs.component';
+import { LogService } from './services/log.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import { LogsComponent } from './components/logs/logs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  // após a criação do LogService é necessário adicioná-lo em providers
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
